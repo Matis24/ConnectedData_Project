076529327face7a9fa666b26f20e3aadcf1d8109
Matis Breillad <mbreillad@gmail.com>
2024-02-07 11:17:11 +0100

    Add gitignore and code to open a servor on project.js

--- code/Project.js
+++ code/Project.js
@@ -1,1 +1,21 @@
+// Creation of the servor
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Data we want to see
+  const message = 'Bonjour, ceci est un serveur local!';
+
+  // Send response
+  res.end(message);
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur http://localhost:${port}/`);
+});
+
 console.log("test")
