a22983bdb1e5ad6c95ed3f43d9b06aef058073b0
Matis Breillad <mbreillad@gmail.com>
2024-02-07 11:48:02 +0100

    Some changes

--- code/Project.js
+++ code/Project.js
@@ -1,13 +1,10 @@
 // Creation of the servor
-const http = require('http');
+import http from 'http';
 
 const server = http.createServer((req, res) => {
-    
   res.writeHead(200, {'Content-Type': 'text/plain'});
-
   // Data we want to see
   const message = 'Bonjour, ceci est un serveur local!';
-
   // Send response
   res.end(message);
 });
@@ -17,5 +14,3 @@ const port = 3000;
 server.listen(port, () => {
   console.log(`Serveur en cours d'exécution sur http://localhost:${port}/`);
 });
-
-console.log("test")
