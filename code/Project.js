db47fe715fe54998133fa3deb43031a2f4a78aa2
Matis Breillad <mbreillad@gmail.com>
2024-02-07 16:24:16 +0100

    Succeed to get data from github API and show on local servor

--- code/Project.js
+++ code/Project.js
@@ -1,12 +1,30 @@
 // Creation of the servor
-import http from 'http';
+const http = require('http');
+import fetch from 'node-fetch';
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  // Data we want to see
-  const message = 'Bonjour, ceci est un serveur local!';
-  // Send response
-  res.end(message);
+
+  try{
+    // Function to get the data with API of PlantNet
+    async function firstAPI(){
+      const response = await fetch("https://raw.githubusercontent.com/erik-sytnyk/movies-list/master/db.json");
+      const data = await response.json();
+      return data;
+    };
+
+    // Wait for the API response and stock it in a variable
+    const data = await firstAPI();
+    
+    const message = JSON.stringify(data.movies[0]);
+
+    // Send response
+    res.end(message);
+  }
+  catch (error){
+    console.error("Error",error);
+    res.end ("Error")
+  }
 });
 
 const port = 3000;
