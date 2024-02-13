65f4513f6371f4db0434d63146031e50236891ea
Matis Breillad <mbreillad@gmail.com>
2024-02-13 22:05:25 +0100

    Added api for plantnet and mistral, as well as dotenv to hide api keys

--- code/Project.js
+++ code/Project.js
@@ -1,5 +1,9 @@
 // Library needed
 const http = require('http');
+require('dotenv').config();
+
+const api_key_plantnet = process.env.API_KEY_PLANTNET;
+const api_key_mistral = process.env.API_KEY_MISTRAL;
 
 // Launch the server
 const server = http.createServer(async (req, res) => {
@@ -7,24 +11,59 @@ const server = http.createServer(async (req, res) => {
 
   try{
     // Function to get the data with API of PlantNet
-    async function firstAPI(){
-      const response = await fetch("https://raw.githubusercontent.com/erik-sytnyk/movies-list/master/db.json");
-      const data = await response.json();
+    async function PlantNetAPI(){
+      const plantnet = await fetch(`https://my-api.plantnet.org/v2/species?api-key=${api_key_plantnet}&lang=en`);
+      const data = await plantnet.json();
       return data;
     };
+    // Wait for the API response and get list of species
+    const data = await PlantNetAPI();
+    const species = data[0];
+    // const speciesId = species[0].id;  // Convert the data in string
 
-    // Wait for the API response and stock it in a variable
-    const data = await firstAPI();
-    
-    const message = JSON.stringify(data.movies[0]);
+
+    // Function to get the data with API of GBIF
+    //  async function gbifAPI(id){
+    //   const gbif = await fetch("https://api.gbif.org/v1/species/id/distributions");
+    //   const data = await gbif.json();
+    //   return data;
+    //   };
+    // const distributionData = await gbifAPI(speciesId);
+
+    // Mistral 
+    // Fonction
+    async function mistralAPI(){
+      const mistral = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${api_key_mistral}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "model": "mistralai/mistral-7b-instruct:free", // Optional (user controls the default),
+          "messages": [
+            {"role": "user", "content": "What is the meaning of life?"},
+          ]
+        })
+      });
+      return mistral
+    }
+
+    const mistralres = await mistralAPI();
 
     // Send response
-    res.end(message);
+
+    res.end(JSON.stringify({
+      message: mistralres,
+      species: species
+    }));
   }
+
   catch (error){
     console.error("Error",error);
     res.end ("Error")
   }
+
 });
 
 const port = 3000;
